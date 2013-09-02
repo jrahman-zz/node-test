88c0605441b96d121124913dbb9d12c487ab00ea
Jason P. Rahman <jprahman93@gmail.com>
2013-09-02 15:25:27 -0400

    Format fix in node-test.js and nginx config change

--- src/node-test.js
+++ src/node-test.js
@@ -9,13 +9,13 @@ http.createServer(function(request, response) {
 
 	process.stdout.write("Recieved request\n");
 
-		// Get the url parameters
-		var _get = url.parse(request.url, true).query;
+	// Get the url parameters
+	var _get = url.parse(request.url, true).query;
 
-		response.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
-		response.end("Hello World! " + _get['echo']);
+	response.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	response.end("Hello World! " + _get['echo']);
 
 }).listen(8080);
 
