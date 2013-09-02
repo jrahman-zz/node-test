172543fcb63757889ec1fce68b93ea9b7e58c9ed
Jason P. Rahman <jprahman93@gmail.com>
2013-09-02 15:22:45 -0400

    Fix the test code

--- src/node-test.js
+++ src/node-test.js
@@ -7,21 +7,15 @@ var url = require('url');
 // Create server to receive and respond to requests
 http.createServer(function(request, response) {
 
-	// Attach a listener for the end event
-	request.on('end', function () {
-		
-		process.stdout.write("Recieved request\n");
+	process.stdout.write("Recieved request\n");
 
 		// Get the url parameters
-		//var _get = url.parse(request.url, true).query;
+		var _get = url.parse(request.url, true).query;
 
 		response.writeHead(200, {
 			'Content-Type': 'text/plain'
 		});
-		response.end("Hello World! "/* + _get['echo']*/);
-
-	});
-
+		response.end("Hello World! " + _get['echo']);
 
 }).listen(8080);
 
