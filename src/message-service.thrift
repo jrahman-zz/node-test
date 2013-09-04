b66c0978bce1174849460ca13ddf609b1a8e40c9
Jason P. Rahman <jprahman93@gmail.com>
2013-09-04 00:33:47 -0400

    Initial commit for thrift message definitions

---
+++ src/message-service.thrift
@@ -0,0 +1,23 @@
+/*
+	Thrift service definition file
+	for <PROJECT NAME HERE>
+
+
+*/
+
+
+Struct Message {
+	1: required string Message;
+	2: required 	
+
+}
+
+
+Struct {
+
+}
+
+
+Service {
+
+}
